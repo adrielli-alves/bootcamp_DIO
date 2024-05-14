09f0394fa3b54e3e1e1ca0af47e732c49bd9c2dd
adrielli <adrielli.crisp@gmail.com>
2024-05-14 08:07:26 -0300

    anatomia-de-classes-4

--- criação-de-arquivo-java/codigos_aula/MinhaClasse.java
+++ criação-de-arquivo-java/codigos_aula/MinhaClasse.java
@@ -1,5 +1,23 @@
 public class MinhaClasse {
     public static void main(String [] args) throws Exception {
-        System.out.print("Hello World");
+        String primeiroNome = "Adrielli";
+        String segundoNome = "Alves";
+        System.out.println(primeiroNome);
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+        
+        
+        int anoNascimento = 2000;
+        boolean casada = true;
+        anoNascimento = 2002;
+        System.out.println("Ano de Nascimento: " + anoNascimento);
+        System.out.println("Casada: " + casada);
+        
+
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Nome completo: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
