b49a38ea46ad4943ca070f50aff921fa51835238
adrielli <adrielli.crisp@gmail.com>
2024-05-14 22:10:38 -0300

    operadores-3

---
+++ criação-de-arquivo-java/operadores/ExemploOperadores.java
@@ -0,0 +1,9 @@
+public class ExemploOperadores {
+    public static void main(String[] args) {
+        int numero = 6;
+        System.out.println(-numero);
+        numero = -6;
+        System.out.println(-numero);
+        System.out.println(--numero);
+    }
+}
