4727bf9b34eeaa71c195b18c53e831e0b4a7911c
adrielli <adrielli.crisp@gmail.com>
2024-05-14 22:47:43 -0300

    metodos

--- criação-de-arquivo-java/metodos/ExemploMetodos.java
+++ criação-de-arquivo-java/metodos/ExemploMetodos.java
@@ -1,5 +1,14 @@
 public class ExemploMetodos {
     public static void main(String[] args) {
-        
+        System.out.println(1);
+    }
+
+    public double dividir (int dividendo, int divisor) throws Exception{
+        return dividendo / divisor;
+    }
+
+
+    public static String concatenar (String a, String b) {
+        return a.concat(b);
     }
 }
