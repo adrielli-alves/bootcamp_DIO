94ddc0ce6e5deb703ba5fd45914b79411f2acc92
adrielli <adrielli.crisp@gmail.com>
2024-05-14 22:29:20 -0300

    operadores

--- criação-de-arquivo-java/operadores/ExemploOperadores.java
+++ criação-de-arquivo-java/operadores/ExemploOperadores.java
@@ -1,9 +1,21 @@
 public class ExemploOperadores {
     public static void main(String[] args) {
         int numero = 6;
-        System.out.println(-numero);
+        System.out.println(numero);
         numero = -6;
         System.out.println(-numero);
-        System.out.println(--numero);
+
+        boolean variavel = true;
+        System.out.println(variavel);
+        System.out.println(!variavel);
+
+        System.out.println(5==6 ? "números iguais" : "números diferentes");
+
+        System.out.println("OI".equals("OI"));
+        
+        if (!(3>4)) {
+            System.out.println("condição correta");
+        }
+    
     }
 }
