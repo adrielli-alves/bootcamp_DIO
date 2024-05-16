487c71c6e2ba1d2c84f155271ed0140675a557f8
adrielli <adrielli.crisp@gmail.com>
2024-05-16 09:30:07 -0300

    conclusao-metodos

--- criação-de-arquivo-java/metodos/SmartTV.java
+++ criação-de-arquivo-java/metodos/SmartTV.java
@@ -1,14 +1,41 @@
 public class SmartTV {
     boolean ligada = false;
     int canal = 1;
-    int volume = 0;
+    int volume = 25;
 
 
     public void ligar() {
         ligada = true;
+        System.out.println("A televisão está ligada");
     }
-
+    
     public void desligar() {
         ligada = false;
+        System.out.println("A televisão está desligada");
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("O volume atual é de " + volume);
+    }
+    
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("O volume atual é de " + volume);
+    }
+    
+    public void aumentarCanal(){
+        canal++;
+        System.out.println("O canal atual é " + canal);
+    }
+    
+    public void diminuirCanal(){
+        canal--;
+        System.out.println("O canal atual é " + canal);
+    }
+    
+    public void mudarCanal (int novoCanal) {
+        canal = novoCanal;
+        System.out.println("O canal atual é " + canal);
     }
 }
--- criação-de-arquivo-java/metodos/Usuario.java
+++ criação-de-arquivo-java/metodos/Usuario.java
@@ -6,5 +6,22 @@ public class Usuario {
         System.out.println("Tv ligada: " + SmartTV.ligada);
         System.out.println("Canal atual: " + SmartTV.canal);
         System.out.println("Volume atual: " + SmartTV.volume);
+        
+        SmartTV.diminuirVolume();
+        SmartTV.diminuirVolume();
+        SmartTV.diminuirVolume();
+        SmartTV.diminuirVolume();
+        SmartTV.aumentarVolume();
+
+        SmartTV.aumentarCanal();
+        SmartTV.aumentarCanal();
+        SmartTV.aumentarCanal();
+        SmartTV.diminuirCanal();
+            
+        SmartTV.desligar();
+
+        SmartTV.ligar();
+
+        SmartTV.mudarCanal(15);
     }
 }
