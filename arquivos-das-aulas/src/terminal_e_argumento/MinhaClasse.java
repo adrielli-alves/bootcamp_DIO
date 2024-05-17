22df91e3ec72cc89d44b67925f31df4d3d4523c4
adrielli <adrielli.crisp@gmail.com>
2024-05-17 13:01:08 -0300

    anatomia-2

---
+++ arquivos-das-aulas/src/terminal_e_argumento/MinhaClasse.java
@@ -0,0 +1,7 @@
+package terminal_e_argumento;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Oi Turma, fui executado pelo Terminal");
+    }
+}
---
+++ arquivos-das-aulas/src/terminal_e_argumento/SobreMim.java
@@ -0,0 +1,22 @@
+package terminal_e_argumento;
+
+public class SobreMim {
+    public static void main(String[] args) {
+        String nome = args [0];
+        String sobrenome = args [1];
+        int idade = Integer.valueOf(args[2]);
+        double altura = Double.valueOf(args[3]);
+
+        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é " + altura + "cm");
+
+        /**
+         * Ao rodar o programa no terminal, é necessário informar os dados do array para que sejam identificados
+         * Exemplo:
+         * Já no cmd, dentro da pasta bin, inserir a informação:
+         * java SobreMim Adrielli Alves 21 1.55
+         */
+    }
+    
+}
