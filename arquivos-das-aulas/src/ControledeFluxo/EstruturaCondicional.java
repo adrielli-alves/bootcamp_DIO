7a112900a957ec431667865a762660f74f1a2bc9
adrielli <adrielli.crisp@gmail.com>
2024-05-20 14:57:00 -0300

    controle-de-fluxo

---
+++ arquivos-das-aulas/src/ControledeFluxo/EstruturaCondicional.java
@@ -0,0 +1,35 @@
+package ControledeFluxo;
+
+public class EstruturaCondicional {
+    public static void main (String[] args) {
+        String plano = "M";
+
+        // If-else
+        if (plano == "B") {
+            System.out.println("100 minutos de ligação");
+        }
+        else if (plano == "M") {
+            System.out.println("100 minutos de ligação");
+            System.out.println("Whatsapp e Instagram grátis");
+        }
+        else if (plano == "T") {
+            System.out.println("100 minutos de ligação");
+            System.out.println("Whatsapp e Instagram grátis");
+            System.out.println("5Gb Youtube");
+        }
+
+        // Switch-case
+        switch (plano) {
+            case "T": {
+                System.out.println("5Gb Youtube");
+            }
+            case "M": {
+                System.out.println("Whatsapp e Instagram grátis");
+            }
+            case "B": {
+                System.out.println("100 minutos de ligação");
+            }
+        }
+    }
+}
+
