94894f5cc14a94dab24572573a139659a51ec835
adrielli <adrielli.crisp@gmail.com>
2024-05-26 18:01:25 -0300

    processo_seletivo

--- arquivos-das-aulas/src/App.java
+++ arquivos-das-aulas/src/App.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String[] lista = { "a", "b", "c" };
+
+        // Converter o array em um ArrayList
+        List<String> listaArrayList = new ArrayList<>(Arrays.asList(lista));
+
+        // Remover o elemento "c"
+        listaArrayList.remove("c");
+
+        // Converter o ArrayList de volta para um array
+        lista = listaArrayList.toArray(new String[0]);
+
+        // Imprimir o resultado
+        System.out.println(Arrays.toString(lista));
     }
 }
