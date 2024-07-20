80b1e14b51343f6b48266a1e3528dafe125239ff
adrielli <adrielli.crisp@gmail.com>
2024-07-20 19:15:37 -0300

    Desatio POO pratica

---
+++ programacao_orientada_a_objetos/src/desafios/codigo_intermediario/Desafio0.java
@@ -0,0 +1,60 @@
+package desafios.codigo_intermediario;
+
+import java.util.Scanner;
+import java.util.Locale;
+import java.text.DecimalFormat;
+
+
+public class Desafio0 {
+    public static void main(String[] args) {
+        // Lendo os dados de Entrada:
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        String titular = scanner.next();
+        int numeroConta = scanner.nextInt();
+        double saldo = scanner.nextDouble();
+        double taxaJuros = scanner.nextDouble();
+
+        ContaPoupanca contaPoupanca = new ContaPoupanca(numeroConta, titular, saldo, taxaJuros);
+
+        System.out.println("Conta Poupanca:");
+        contaPoupanca.exibirInformacoes();
+
+        // Fechar o scanner para evitar vazamentos de recursos
+        scanner.close();
+    }
+}
+
+class ContaBancaria01 {
+    protected int numero;
+    protected String titular;
+    protected double saldo;
+
+    public ContaBancaria01(int numero, String titular, double saldo) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public void exibirInformacoes() {
+        DecimalFormat decimalFormat = new DecimalFormat("#.0");
+        System.out.println(titular);
+        System.out.println(numero);
+        System.out.println("Saldo: R$ " + decimalFormat.format(saldo));
+    }
+}
+
+class ContaPoupanca extends ContaBancaria01 {
+    private double taxaJuros;
+
+    public ContaPoupanca(int numero, String titular, double saldo, double taxaJuros) {
+        super(numero, titular, saldo);
+        this.taxaJuros = taxaJuros;
+    }
+
+
+    public void exibirInformacoes() {
+        super.exibirInformacoes();
+        DecimalFormat decimalFormat = new DecimalFormat("#.0");
+        System.out.println("Taxa de juros: " + decimalFormat.format(taxaJuros) + "%");
+    }
+}
