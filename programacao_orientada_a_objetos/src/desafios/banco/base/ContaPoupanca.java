77953b3ca66966166db1eda6a35884ff65acb0c7
adrielli <adrielli.crisp@gmail.com>
2024-07-20 19:17:33 -0300

    desafios

---
+++ programacao_orientada_a_objetos/src/desafios/banco/base/ContaPoupanca.java
@@ -0,0 +1,14 @@
+package desafios.banco.base;
+
+public class ContaPoupanca extends Conta {
+
+	public ContaPoupanca(Cliente cliente) {
+		super(cliente);
+	}
+
+	@Override
+	public void imprimirExtrato() {
+		System.out.println("=== Extrato Conta Poupan�a ===");
+		super.imprimirInfosComuns();
+	}
+}
